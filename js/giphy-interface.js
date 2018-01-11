0b5061bad7bcef294579cf503a2c3fe3bc6a533a
Cat Janowitz and Kevin Boyle <cat@>
2018-01-10 16:03:09 -0800

    use class to instantiate object and get topic with output giphy array

--- js/giphy-interface.js
+++ js/giphy-interface.js
@@ -1,37 +1,40 @@
+const apiKey = require('./../.env').apiKey;
+import {GiphyLand} from './../js/giphy.js';
+
 $(document).ready(function() {
   $('form#search').submit(function(event) {
     event.preventDefault();
     $('.result').empty();
     const gifs = $('#gif-search-input').val();
     $('#gif-search-input').val("");
-    $.ajax({
-      url: `http://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=BfYSecfMXlpv1i9L4XB4HChFSR8T59jo&limit=50`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(thing) {
-        // let giphyArr = returned JSON file
-        // giphyArr.splice((Math.floor(Math.random() * 20) + 1), 5)
-        let a = Math.floor(Math.random() * 45) + 1;
-        let b = a + 5;
-        // let c = a - 5;
-        // if (a <= 15) {
-          for (var i = a; i < b; i++){
-            console.log(i + "i");
-            $('.result').append(`<img src="${thing.data[i].images.fixed_height.url}">`);
-          }
-        // } else if (a > 15) {
-        //     for (var i = a; i > c; i--) {
-        //     console.log(i, a, c)
-        //     $('.result').append(`<img src="${thing.data[i].images.fixed_height.url}">`);
-        //   };
-        // };
+    const newRequest = new GiphyLand(gifs);
+  });
+});
 
-    },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
-    }); //ajax callback
-  }); //form submit
-}); //doc ready
+// COPY OF CODE:
+// $(document).ready(function() {
+//   $('form#search').submit(function(event) {
+//     event.preventDefault();
+//     $('.result').empty();
+//     const gifs = $('#gif-search-input').val();
+//     $('#gif-search-input').val("");
+//     $.ajax({
+//       url: `http://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=${apiKey}&limit=50`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(thing) {
+//         let a = Math.floor(Math.random() * 45) + 1;
+//         let b = a + 5;
+//           for (var i = a; i < b; i++){
+//             console.log(i + "i");
+//             $('.result').append(`<img src="${thing.data[i].images.fixed_height.url}">`);
+//           }
+//     },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.")
+//       }
+//     }); //ajax callback
+//   }); //form submit
+// }); //doc ready
---
+++ js/giphy.js
@@ -0,0 +1,34 @@
+const apiKey = require('./../.env').apiKey;
+
+export class GiphyLand {
+  constructor(inputTopic, outputGifs) {
+    this.inputTopic = inputTopic;
+    this.outputGifs = this.getGiphs();
+  }
+
+  getGiphs(){
+    $.ajax({
+      url: `http://api.giphy.com/v1/gifs/search?q=${this.inputTopic}&api_key=${apiKey}&limit=50`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        let a = Math.floor(Math.random() * 45) + 1;
+        let b = a + 5;
+        for (var i = a; i < b; i++){
+          console.log(i + "i");
+          $('.result').append(`<img src="${response.data[i].images.fixed_height.url}">`);
+        }
+
+        // complete: function(){
+        //
+        // }
+
+      },//success
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });//ajax
+  } //getGiphs()
+}//GetGiphy
